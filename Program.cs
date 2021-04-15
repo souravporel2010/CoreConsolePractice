572ff551c0393d8784fb5306237eca7128d4d4c5
souravporel <souravporel.2010@gmail.com>
2021-04-15 11:32:58 +0530

    1st

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace CoreConsolePractice
         static void Main(string[] args)
         {
             Console.WriteLine("Counter Value- "+ StaticTest.counter);
+            Console.ReadLine();
         }
 
     }
