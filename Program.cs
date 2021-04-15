105edc735f35d40fc28193945dc6a622fd71eed5
souravporel <souravporel.2010@gmail.com>
2021-04-15 11:31:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CoreConsolePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Counter Value- "+ StaticTest.counter);
+        }
+
+    }
+
+    class StaticTest
+    {
+        public static int counter;
+    }
+}
