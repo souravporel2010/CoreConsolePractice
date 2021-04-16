36593d746c753a0802abcbd2af742a23bf75502a
souravporel <souravporel.2010@gmail.com>
2021-04-16 13:31:11 +0530

    initial

--- EventDemo.cs
+++ EventDemo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CoreConsolePractice
@@ -11,6 +12,54 @@ namespace CoreConsolePractice
         public static void Main(string[] args)
         {
             Console.WriteLine("----My Event Demo----");
+            //MailService ms = new MailService();
+
+            JobService js = new JobService();
+            js.jobhandler += MailService.MailSending;
+            js.jobhandler += MessageService.MessageSending;
+            js.JobRunning();
+
         }
+
+        public class JobService
+        {
+            public delegate void JobEventHandler();
+
+            public JobEventHandler jobhandler;
+
+            public void JobRunning()
+            {
+                Console.WriteLine("Job Running---");
+                Thread.Sleep(3000);
+                Console.WriteLine("Job Completed");
+                JobEventPublisher();
+            }
+
+            public void JobEventPublisher()
+            {
+                if (jobhandler != null)
+                {
+                    jobhandler();
+                }
+
+            }
+        }
+
+        public class MailService 
+        {
+            public static void MailSending()
+            {
+                Console.WriteLine("Mail sended");
+            }
+        }
+
+        public class MessageService
+        {
+            public static void MessageSending()
+            {
+                Console.WriteLine("Message sended");
+            }
+        }
+
     }
 }
