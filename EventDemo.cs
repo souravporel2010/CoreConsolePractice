11a3355d4d2c7c737752cd8b6ae85ab69424837f
souravporel <souravporel.2010@gmail.com>
2021-04-16 13:14:34 +0530

    new file

---
+++ EventDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreConsolePractice
+{
+    class EventDemo
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("----My Event Demo----");
+        }
+    }
+}
